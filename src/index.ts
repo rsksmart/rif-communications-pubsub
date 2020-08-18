ee759fdc0587e063af9b8debf92020b3a2e489a2
Adam Uhlíř <adam@uhlir.dev>
2020-08-18 10:20:30 +0200

    chore: export types

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import DirectRoom from './room/direct'
 import createLibP2P from './libp2p/nodejs'
 
 export { Room, DirectRoom, createLibP2P }
+export type { Message } from './room/Message'
+export type { Options as RoomOptions } from './room/pubsub'
